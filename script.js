b64920a57f47e8c146498086ef30b1a6b2b95eda
mrmuhib300@gmail.com <mrmuhib300@gmail.com>
2024-10-23 18:38:53 +0600

    recover asign value

--- script.js
+++ script.js
@@ -1,4 +1,31 @@
+// const display = document.getElementById("display");
+
+// const appendToDisplay = (e) => {
+//   display.value += e;
+// };
+
+// const clearDisplay = () => {
+//   display.value = "";
+// };
+
+// const deleteLast = () => {
+//   display.value = display.value.slice(0, -1);
+// };
+
+// const appendToDisplaySimble = (simble) => {
+//   display.value += simble;
+// };
+
+// const calculateResult = () => {
+//   try {
+//     display.value = eval(display.value);
+//   } catch (error) {
+//     display.value = "Error";
+//   }
+// };
+
 const display = document.getElementById("display");
+const arr = ["+", "-", "*", "/"];
 
 const appendToDisplay = (e) => {
   display.value += e;
@@ -11,9 +38,16 @@ const clearDisplay = () => {
 const deleteLast = () => {
   display.value = display.value.slice(0, -1);
 };
-const appendToDisplaySimble = (simble) => {
-  display.value = display.value + simble;
-  display.value = eval(display.value);
+
+const appendToDisplaySimble = (symbol) => {
+  const isSymbol = arr.includes(display.value[display.value.length - 1]);
+  const temp = display.value.slice(0, display.value.length - 1) + symbol;
+
+  if (!isSymbol) {
+    display.value = display.value + symbol;
+  } else {
+    display.value = temp;
+  }
 };
 
 const calculateResult = () => {
